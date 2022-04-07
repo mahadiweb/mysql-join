bcc00017eb4a01b77a2fdb06e59c21406f52575d
Md.Mahadi Hasan <64768414+mahadiweb@users.noreply.github.com>
2022-04-07 23:00:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_connect("localhost","root","","join");
 
-$sql = "SELECT tbl1.name AS tblf,tbl1.email,tbl2.name FROM tbl1,tbl2 WHERE tbl1.id = tbl2.main_id"; //this is type of inner join
+$sql = "SELECT tbl1.name AS tblf,tbl1.email,tbl2.name,tbl1.*,tbl2.test FROM tbl1,tbl2 WHERE tbl1.id = tbl2.main_id"; //this is type of inner join
 //SELECT (tablename + rowname) AS (set a short name of row using AS) FROM (enter table name which you used for join use between ',') WHERE (add tablename+rowname = tablename+rowname For join same id or etc);
 $query = mysqli_query($conn, $sql);
 
